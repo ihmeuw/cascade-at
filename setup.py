8fffa6b8c4fe15e8c37266e0647597a2b1e1afbe
Natalie Hawkins <nhawkins@uw.edu>
2018-07-19 17:05:28 -0700

    wip: moved files for argparse, recipe, parallel; tests pass when have __init__.py in cascade, which we don't want for a namespace pkg

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ setup(
     name="cascade",
     version="0.0.1",
     packages=PEP420PackageFinder.find("src"),
+    package_data={"cascade": [
+        "data/*.toml",
+    ]},
     package_dir={"": "src"},
     include_package_data=True,
-    install_requires=["pandas"],
+    install_requires=["pandas", "toml"],
     extras_require={
         "testing": ["pytest", "pytest-mock", "hypothesis"],
         "ihme_databases": ["db_tools"],
