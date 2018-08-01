dbd4bcac50efac5ff4c0fae8efb5b6e264f6719a
Andrew Dolgert <adolgert@uw.edu>
2018-08-01 13:15:37 -0700

    autodoc not working yet

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.expanduser("..")))
+sys.path.insert(0, os.path.abspath(os.path.expanduser("..\src")))
 
 
 # -- General configuration ------------------------------------------------
