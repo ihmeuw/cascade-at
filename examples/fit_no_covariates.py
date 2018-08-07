bbc91722e8277c432e0e2451d746a541f320134b
Andrew Dolgert <adolgert@uw.edu>
2018-08-06 17:06:55 -0700

    have separate input, internal, and output models

--- examples/fit_no_covariates.py
+++ examples/fit_no_covariates.py
@@ -21,6 +21,8 @@ years.
 .. autofunction: observe_demographic_rates
 
 """
+from argparse import Namespace
+import itertools as it
 import logging
 from pathlib import Path
 import pickle
@@ -28,95 +30,13 @@ from timeit import default_timer as timer
 
 import numpy as np
 import pandas as pd
-from scipy.stats import gamma
 
-from cascade.model import (
-    siler_default, prevalence_solution, integrands_from_function, omega_from_mu,
-    DemographicInterval
-)
 from cascade.input_data.db.bundle import bundle_with_study_covariates
 from cascade.dismod.db.wrapper import DismodFile, _get_engine
 from cascade.testing_utilities import make_execution_context
-from cascade.dismod.db.metadata import IntegrandEnum
+from cascade.dismod.db.metadata import IntegrandEnum, DensityEnum
 
 LOGGER = logging.getLogger("fit_no_covariates")
-
-
-def pretend_diabetes():
-    """
-    Create theoretical distributions for a disease. Make just enough to
-    specify the problem. Then let the library calculate what those imply.
-    In this case, we loaded diabetes data from the United States in 2015
-    and made, by hand, rates of disease that sort-of match the age pattern
-    of diabetes in that year.
-    """
-    def diabetes_incidence(x):
-        return 0.8 * gamma(a=9, scale=7).pdf(x)
-
-    def diabetes_emr(x):
-        """Create an excess mortality rate."""
-        return 0.015 * (np.exp((x / 100) ** 2) - 1)
-
-    def diabetes_remission(x):
-        return np.zeros_like(x)
-
-    total_mortality = siler_default()
-
-    # Use those rates to parameterize a differential equation, which we then
-    # solve to get susceptibles, with-condition, and prevalence = S/(S+C)
-    # as functions of cohort time.
-    S, C, P = prevalence_solution(
-        diabetes_incidence, diabetes_remission, diabetes_emr, total_mortality)
-
-    # We need N=S+C, which is the number of people alive. It's also known
-    # as :math:`l_x(t)` in demography terms. This will be necessary to
-    # create observations on a theoretical population.
-    def lx(t):
-        return S(t) + C(t)
-
-    omega = omega_from_mu(total_mortality, diabetes_emr, P)
-
-    return dict(
-        incidence=diabetes_incidence,
-        emr=diabetes_emr,
-        remission=diabetes_remission,
-        total_mortality=total_mortality,
-        other_mortality=omega,
-        susceptible=S,
-        with_condition=C,
-        prevalence=P,
-        lx=lx,
-    )
-
-
-def observe_demographic_rates(rates, ages):
-    """
-    Given continuous functions, return rates as averages over intervals,
-    done in the same way they would be observed for populations. This means
-    they are averaged over :math:`l_x`, the number of people alive across
-    the interval.
-
-    Args:
-        rates (dict[str,function]): A list of functions of cohort-time, defined
-            for all times in the age intervals.
-        ages (DemographicInterval): Age intervals for which to predict
-            results.
-
-    Returns:
-        list(np.array): A list of estimated integrands on the age intervals.
-    """
-    rates, norm = integrands_from_function(
-        [rates["incidence"], rates["with_condition"]],
-        rates["lx"],
-        ages,
-    )
-    return rates + [norm]
-
-
-def theory():
-    rates = pretend_diabetes()
-    intervals = DemographicInterval(np.full((10,), 10.0, dtype=np.float))
-    observations = observe_demographic_rates(rates, intervals)
 
 
 def cached_bundle_load(context, bundle_id, tier_idx):
@@ -138,7 +58,8 @@ def cached_bundle_load(context, bundle_id, tier_idx):
     return bundle, covariate
 
 
-def bundle_to_observations(bundle_df):
+def bundle_to_observations(config, bundle_df):
+    """Convert bundle into an internal format."""
     for check_measure in bundle_df["measure"].unique():
         if check_measure not in IntegrandEnum.__members__:
             raise KeyError(f"{check_measure} isn't a name known to Cascade.")
@@ -146,75 +67,207 @@ def bundle_to_observations(bundle_df):
     if "location_id" in bundle_df.columns:
         location_id = bundle_df["location_id"]
     else:
-        location_id = np.full(len(bundle_df), -1, dtype=np.int)
+        location_id = np.full(len(bundle_df), config.location_id, dtype=np.int)
+
+    demographic_interval_specification = 1
+
     return pd.DataFrame({
         "measure": bundle_df["measure"],
         "location_id": location_id,
+        "density_id": DensityEnum.gaussian,
+        "weight_id": "constant",
         "age_start": bundle_df["age_start"],
-        "age_end": bundle_df["age_end"],
+        "age_end": bundle_df["age_end"] + demographic_interval_specification,
         "year_start": bundle_df["year_start"].astype(np.float),
-        "year_end": bundle_df["year_end"].astype(np.float),
+        "year_end": bundle_df["year_end"].astype(np.float) + demographic_interval_specification,
         "mean": bundle_df["mean"],
         "standard_error": bundle_df["standard_error"],
     })
 
 
-def observations_to_integrand(bundle_df, node_id):
+def observations_to_data(dismodel, observations_df):
+    """Turn an internal format into a Dismod format."""
     measure_to_integrand = dict(
         incidence=IntegrandEnum.Sincidence.value,
         mtexcess=IntegrandEnum.mtexcess.value,
     )
     return pd.DataFrame({
-        "measure": bundle_df["measure"].apply(measure_to_integrand.get),
-        "node_id": bundle_df["location_id"],
-        "age_lower": bundle_df["age_start"],
-        "age_upper": bundle_df["age_end"],
-        "time_lower": bundle_df["year_start"].astype(np.float),
-        "time_upper": bundle_df["year_end"],
-        "meas_value": bundle_df["mean"],
-        "meas_std": bundle_df["standard_error"],
-        "hold_out": [0] * len(bundle_df),
+        "measure": observations_df["measure"].apply(measure_to_integrand.get),
+        # Translate node id from location_id
+        "node_id": observations_df["location_id"],
+        # Translate density from string
+        "density_id": observations_df["density_id"],
+        # Translate weight from string
+        "weight_id": observations_df["weight_id"],
+        "age_lower": observations_df["age_start"],
+        "age_upper": observations_df["age_end"],
+        "time_lower": observations_df["year_start"].astype(np.float),
+        "time_upper": observations_df["year_end"],
+        "meas_value": observations_df["mean"],
+        "meas_std": observations_df["standard_error"],
+        "hold_out": 0,
     })
 
 
-class DismodModel:
-    def __init__(self):
-        pass
-
-    def add_observations(self, observations):
-        """An observation is a data value that has observational error."""
-        self.observations.append(observations)
-
-    def add_constraint(self, constraint):
-        """A constraint is a data value the fit should assume is true."""
-        self.constraints.append(constraint)
-
-    def write(self, dismod_file):
-        
-        dismod_file.avgint = integrands
-
-def fill_dismod_file(bundle):
-    pass
+def choose_constraints(bundle, measure):
+    observations = pd.DataFrame(bundle[bundle["measure"] != measure])
+    constraints = pd.DataFrame(bundle[bundle["measure"] == measure])
+    return observations, constraints
 
 
-def construct_database():
+def age_time_from_data(df):
+    results = dict()
+    for topic in ["age", "time"]:
+        values = np.unique(np.hstack([df[f"{topic}_start"], df[f"{topic}_end"]]))
+        values.sort()
+        results[topic] = pd.DataFrame({topic: values})
+    return results["age"], results["time"]
+
+
+def default_integrand_names():
+    # Converting an Enum to a DataFrame with specific parameters
+    integrands = enum_to_dataframe(IntegrandEnum)
+    df = pd.DataFrame({"integrand_name": integrands["name"]})
+    df["minimum_meas_cv"] = 0.0
+    return df
+
+
+def enum_to_dataframe(enum_name):
+    """Given an enum, return a dataframe with two columns, name and value."""
+    return pd.DataFrame.from_records(
+        np.array(
+            [(measure, enum_value.value) for (measure, enum_value) in enum_name.__members__.items()],
+            dtype=np.dtype([('name', object), ('value', np.int)])
+        )
+    )
+
+
+def simplest_weight():
+    weight = pd.DataFrame({
+        "weight_name": ["constant"],
+        "n_age": [1],
+        "n_time": [1],
+    })
+    weight_grid = pd.DataFrame({
+        "weight_id": [0],
+        "age_id": [0],
+        "time_id": [0],
+        "weight": [1.0],
+    })
+    return weight, weight_grid
+
+
+RATE_TO_INTEGRAND = dict(
+    iota="Sincidence",
+    rho="remission",
+    chi="mtexcess",
+    omega="mtother",
+    prevalence="prevalence",
+)
+
+
+def integrand_outputs(rates, age, time):
+    age_time = np.array(list(it.product(age, time)))
+    entries = list()
+    for rate in (RATE_TO_INTEGRAND.get(r) for r in rates):
+        entries.append(pd.DataFrame({
+            "integrand": rate,
+            "location_id": 26,
+            "weight": "constant",
+            "age_lower": age_time[:, 0],
+            "age_upper": age_time[:, 0],
+            "time_lower": age_time[:, 1],
+            "time_upper": age_time[:, 1]
+        }))
+    return pd.concat(entries, ignore_index=True)
+
+
+def retrieve_external_data(config):
     context = make_execution_context()
-    bundle_id = 3209
-    tier_idx = 2
-    bundle, covariate = cached_bundle_load(context, bundle_id, tier_idx)
-    integrands = bundle_to_integrand(bundle, 0)
+    bundle, covariate = cached_bundle_load(context, config.bundle_id, config.tier_idx)
+    # Split the input data into observations and constraints.
+    bundle_observations, bundle_constraints = choose_constraints(bundle, "mtother")
+    return Namespace(observations=bundle_observations, constraints=bundle_constraints)
 
+
+def internal_model(config, inputs):
+    # convert the observations to a normalized format.
+    observations = bundle_to_observations(inputs.observations)
+
+    age_df, time_df = age_time_from_data(inputs.constraints)
+    desired_outputs = integrand_outputs(config.options["non_zero_rates"].split(), age_df, time_df)
+    return Namespace(observations=observations, outputs=desired_outputs)
+
+
+def write_to_file(config, model):
     avgint_columns = dict()
     data_columns = dict()
-    bundle_dismod_db = Path("theory.db")
+    bundle_dismod_db = Path("fit_no.db")
     bundle_file_engine = _get_engine(bundle_dismod_db)
     bundle_fit = DismodFile(bundle_file_engine, avgint_columns, data_columns)
 
-    bundle_fit.avgint = integrands
+    # Standard Density table.
+    density_enum = enum_to_dataframe(DensityEnum)
+    densities = pd.DataFrame({"density_name": density_enum["name"]})
+    bundle_fit.density = densities
+
+    # Standard integrand naming scheme.
+    all_integrands = default_integrand_names()
+    bundle_fit.integrands = all_integrands
+
+    # Assume we have one location, so no parents.
+    unique_locations = model.observations["location_id"].unique()
+    node_table = pd.DataFrame({
+        "node_name": unique_locations.astype(int).astype(str),
+        "parent": None,
+    })
+
+    bundle_fit.age = model.age
+    bundle_fit.time = model.time
+
+    # These are helpers to convert from ages and times to age and time indexes.
+    # pd.merge_asof will do an approximate merge.
+    age_idx = pd.DataFrame(model.age)
+    age_idx["index0"] = age_idx.index
+
+    time_idx = pd.DataFrame(model.time)
+    time_idx["index0"] = time_idx.index
+
+    bundle_fit.node = node_table
+    bundle_fit.weight, bundle_fit.weight_grid = simplest_weight()
+
+    # The avgint needs to be translated.
+    bundle_fit.avgint = model.outputs
+
+    observations = observations_to_data(bundle_fit, model.observations)
+    constraints = model.constraints
+    bundle_fit.data = pd.concat([observations, constraints], ignore_index=True)
+
+    bundle_fit.covariate = pd.DataFrame({
+        "covariate_name": np.array(0, object),
+        "reference": np.array(0, np.float),
+        "max_difference": np.array(0, np.float),
+    })
 
     flush_begin = timer()
     bundle_fit.flush()
-    LOGGER.debug(f"Write db {timer() - flush_begin}")
+    LOGGER.debug(f"Flush db {timer() - flush_begin}")
+
+
+def construct_database():
+    # Configuration
+    config = Namespace()
+    config.bundle_id = 3209
+    config.tier_idx = 2
+    config.location_id=26
+    config.options = dict(
+        non_zero_rates = "iota rho chi omega"
+    )
+
+    # Get the bundle and process it.
+    inputs = retrieve_external_data(config)
+    model = internal_model(config, inputs)
+    write_to_file(config, model)
 
 
 if __name__ == "__main__":
