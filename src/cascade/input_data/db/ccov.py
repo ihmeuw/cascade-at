940651aec9bd1ce7d359d0a21d2c2b9a09d38e6b
Natalie Hawkins <nhawkins@uw.edu>
2018-09-07 15:04:34 -0700

    PR change to get data for only 1 covariate_id

--- src/cascade/input_data/db/ccov.py
+++ src/cascade/input_data/db/ccov.py
@@ -3,8 +3,6 @@
 
 import logging
 
-import pandas as pd
-
 try:
     from db_queries import get_covariate_estimates
 except ImportError:
@@ -20,11 +18,11 @@ CODELOG = logging.getLogger(__name__)
 MATHLOG = logging.getLogger(__name__)
 
 
-def country_covariates(execution_context, demographics):
+def country_covariates(covariate_id, demographics):
     """Retrieve country covariates from the database.
 
     Args:
-        execution_context (ExecutionContext): The context within which to make the query
+        covariate_id (int): id of the country covariate to retrieve
         demographics (Dictionary): demographic ids needed to select the data; for example,
             keys for location_ids, age_group_ids, year_ids, and sex_ids.  The values can
             be int or list of ints.
@@ -34,24 +32,15 @@ def country_covariates(execution_context, demographics):
 
     """
 
-    country_covariate_ids = execution_context.parameters.country_covariate_ids
-
-    country_covariates = list()
-
-    for covariate_id in country_covariate_ids:
-
-        country_covariates.append(get_covariate_estimates(
-            covariate_id,
-            location_id=demographics["location_ids"],
-            age_group_id=demographics["age_group_ids"],
-            year_id=demographics["year_ids"],
-            sex_id=demographics["sex_ids"]
-        ))
-
-    ccov = pd.concat(country_covariates, ignore_index=True)[[
-        "covariate_id", "location_id", "age_group_id", "year_id", "sex_id", "mean_value"]]
+    country_covariates = get_covariate_estimates(
+        covariate_id,
+        location_id=demographics["location_ids"],
+        age_group_id=demographics["age_group_ids"],
+        year_id=demographics["year_ids"],
+        sex_id=demographics["sex_ids"])[
+        ["covariate_id", "location_id", "age_group_id", "year_id", "sex_id", "mean_value"]]
 
     CODELOG.debug(
-        f"Downloaded {len(ccov)} lines of country covariates data")
+        f"Downloaded {len(country_covariates)} lines of country covariates data")
 
-    return ccov
+    return country_covariates
--- tests/input_data/db/test_ccov.py
+++ tests/input_data/db/test_ccov.py
@@ -15,7 +15,7 @@ def mock_ccov_estimates():
     mock_ccov_estimates = pd.DataFrame(columns=[
         "covariate_id", "location_id", "age_group_id", "year_id",
         "sex_id", "mean_value"])
-    mock_ccov_estimates.loc[0] = [26, 101, 22, 1990, 1, 1000]
+    mock_ccov_estimates.loc[0] = [33, 101, 22, 1990, 1, 1000]
     return mock_ccov_estimates
 
 
@@ -24,9 +24,7 @@ def expected_ccov():
     expected_ccov = pd.DataFrame(columns=[
         "covariate_id", "location_id", "age_group_id", "year_id",
         "sex_id", "mean_value"])
-    expected_ccov.loc[0] = [26, 101, 22, 1990, 1, 1000]
-    expected_ccov.loc[1] = [26, 101, 22, 1990, 1, 1000]
-    expected_ccov.loc[2] = [26, 101, 22, 1990, 1, 1000]
+    expected_ccov.loc[0] = [33, 101, 22, 1990, 1, 1000]
 
     return expected_ccov
 
@@ -55,14 +53,16 @@ def demographics_default():
 
 
 @pytest.mark.skip
-def test_country_covariates_real(mock_execution_context, demographics_default):
+def test_country_covariates_real(demographics_default):
 
-    ccov = country_covariates(mock_execution_context, demographics_default)
+    country_covariate_id = 26
+
+    ccov = country_covariates(country_covariate_id, demographics_default)
 
     assert set(ccov.columns) == {"covariate_id", "location_id", "age_group_id",
                                  "year_id", "sex_id", "mean_value"}
 
-    assert set(ccov["covariate_id"].unique()) == {26, 28, 33}
+    assert set(ccov["covariate_id"].unique()) == {26}
     assert set(ccov["location_id"].unique()) == {102}
     assert set(ccov["sex_id"].unique()) == {1, 2}
     assert set(ccov["age_group_id"].unique()) == {
@@ -70,24 +70,23 @@ def test_country_covariates_real(mock_execution_context, demographics_default):
         7, 8, 9, 10, 11,
         12, 13, 14, 15, 16,
         17, 18, 19, 20, 30,
-        31, 32, 235, 27}
+        31, 32, 235}
     assert set(ccov["sex_id"].unique()) == {1, 2}
     assert set(ccov["year_id"].unique()) == {
         1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989,
         1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
         2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-        2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 1950, 1951,
-        1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961,
-        1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971,
-        1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979}
+        2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017}
 
 
 def test_country_covariates_mock(
-        mock_execution_context, mock_get_covariate_estimates,
-        mock_ccov_estimates, expected_ccov, demographics_default):
+        demographics_default, mock_get_covariate_estimates,
+        mock_ccov_estimates, expected_ccov):
 
     mock_get_covariate_estimates.return_value = mock_ccov_estimates
 
+    country_covariate_id = 33
+
     pd.testing.assert_frame_equal(
-        country_covariates(mock_execution_context, demographics_default),
+        country_covariates(country_covariate_id, demographics_default),
         expected_ccov, check_like=True)
