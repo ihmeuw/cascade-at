f4f3eb9e938acef42494fda27ae2b50d18317270
Alec Deason <alecwd@uw.edu>
2018-09-10 16:13:14 -0700

    Add test I forgot

---
+++ tests/input_data/configuration/test_form.py
@@ -0,0 +1,24 @@
+from cascade.input_data.configuration.form import SmoothingPrior
+from cascade.model import priors
+
+
+def test_SmoothingPrior__full_form_validation__success():
+    f = SmoothingPrior({"prior_type": "value", "density": "uniform", "min": 0, "max": 10})
+    assert not f.validate_and_normalize()
+    assert f.prior_object == priors.UniformPrior(0, 10)
+
+    f = SmoothingPrior({"prior_type": "value", "density": "log_gaussian", "mean": 0, "std": 1, "eta": 1})
+    assert not f.validate_and_normalize()
+    assert f.prior_object == priors.LogGaussianPrior(0, 1, 1)
+
+
+def test_SmoothingPrior__full_form_validation__fail():
+    f = SmoothingPrior({"prior_type": "value", "density": "uniform", "min": 10, "max": 0})
+    assert f.validate_and_normalize() == [
+        ("", "Parameters incompatible with density 'uniform': Bounds are inconsistent")
+    ]
+
+    f = SmoothingPrior({"prior_type": "value", "density": "gaussian", "mean": 0, "std": -1})
+    assert f.validate_and_normalize() == [
+        ("", "Parameters incompatible with density 'gaussian': Standard deviation must be positive")
+    ]
