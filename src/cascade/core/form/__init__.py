e9df46330e2d5eac8fcb77f507a16da9a6dabe7b
Alec Deason <alecwd@uw.edu>
2018-12-07 11:58:21 -0800

    Add a field type for literal lists rather than string encoded lists

--- src/cascade/core/form/__init__.py
+++ src/cascade/core/form/__init__.py
@@ -1,7 +1,27 @@
 from cascade.core.form.abstract_form import Form
-from cascade.core.form.fields import IntField, FloatField, StrField, FormList, OptionField, StringListField, Dummy
+from cascade.core.form.fields import (
+    IntField,
+    FloatField,
+    StrField,
+    FormList,
+    OptionField,
+    StringListField,
+    ListField,
+    Dummy,
+)
 
 from cascade.core.log import getLoggers
+
 CODELOG, MATHLOG = getLoggers(__name__)
 
-__all__ = ["Form", "IntField", "FloatField", "StrField", "FormList", "OptionField", "StringListField", "Dummy"]
+__all__ = [
+    "Form",
+    "IntField",
+    "FloatField",
+    "StrField",
+    "FormList",
+    "OptionField",
+    "StringListField",
+    "ListField",
+    "Dummy",
+]
