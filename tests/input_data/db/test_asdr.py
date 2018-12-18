8df4279cb9d18472071ce18b5b78d1205dab0ba2
Alec Deason <alecwd@uw.edu>
2018-12-18 14:28:35 -0800

    Fix broken test

--- tests/input_data/db/test_asdr.py
+++ tests/input_data/db/test_asdr.py
@@ -14,7 +14,7 @@ def test_load_asdr_to_t3_did_upload(mock_execution_context, mock_database_access
     cursor = mock_database_access["cursor"]
     model_version_id = mock_execution_context.parameters.model_version_id
 
-    mock_upload_asdr_data.assert_called_with(cursor, model_version_id, mock_get_asdr_data())
+    mock_upload_asdr_data.assert_called_with(mock_execution_context, cursor, model_version_id, mock_get_asdr_data())
 
 
 def test_load_asdr_to_t3_no_upload(mock_execution_context, mock_database_access, mocker):
