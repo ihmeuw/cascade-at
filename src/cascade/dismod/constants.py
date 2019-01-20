1edc2856449e3d4dd88e6116612eb7d3b9b4ae2f
Andrew Dolgert <adolgert@uw.edu>
2019-01-20 12:02:20 -0800

    The Session automatically re-reads tables written by any Dismod-AT command.

--- src/cascade/dismod/constants.py
+++ src/cascade/dismod/constants.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from enum import Enum
 
 
@@ -123,3 +124,27 @@ the differential equation needs to be solved for each cohort in the
 ODE grid that intersects or surrounds the age-time span for this point. This
 computation is performed each time the model variables change.
 """
+
+
+DismodIO = namedtuple("CommandIO", "input output")
+"""Each Dismod-AT command reads and writes subsets of tables.
+These are those tables."""
+
+COMMAND_IO = dict(
+    init=DismodIO(
+        ["age", "avgint", "covariate", "data", "density", "integrand",
+         "mulcov", "node", 'nslist_pair', "nslist", "option",
+         "prior", "rate", "smooth_grid", "smooth", "time",
+         "weight_grid", "weight"],
+        ["age_avg", "var", "data_subset", "start_var", "scale_var"]),
+    depend=DismodIO([], ["depend_var", "age_avg"]),
+    set=DismodIO(
+        ["sample", "prior_mean", "fit_var", "start_var", "scale_var", "truth_var"],
+        ["start_var", "scale_var", "truth_var"]),
+    fit=DismodIO(["data_sim"], ["fit_var", "fit_data_subset", "age_avg"]),
+    simulate=DismodIO(["truth_var"], ["data_sim", "age_avg"]),
+    sample=DismodIO(["fit_var", "data_sim", "prior_sim"],
+                    ["sample", "age_avg"]),
+    predict=DismodIO(["sample", "fit", "truth"], ["predict", "age_avg"]),
+)
+"""Given a command, what tables does it read and write?"""
