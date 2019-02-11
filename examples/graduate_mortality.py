ecbf3398936c323756f03d1b01aefacd76d54d86
Andrew Dolgert <adolgert@uw.edu>
2019-02-10 23:18:55 -0800

    made a pass at the priors-setting from the parent

--- examples/graduate_mortality.py
+++ examples/graduate_mortality.py
@@ -255,6 +255,36 @@ def set_priors_from_draws(model, draws):
             estimate_grid_parameters(prior_grid.dtime, draw_dtime, ages, times[:-1])
 
 
+def set_priors_from_parent_draws(model, draws):
+    """Sets priors from posteriors of the *parent model*."""
+    assert len(draws) > 0
+
+    for group_name, group in model.items():
+        if group_name not in draws[0] or group_name == "random_effect":
+            continue
+
+        for key, prior_grid in group.items():
+            if key not in draws[0][group_name]:
+                continue
+
+            ages = prior_grid.ages
+            times = prior_grid.times
+            if group_name == "rate" and (key, model.location_id) in draws[0]["random_effect"]:
+                draw_value, draw_dage, draw_dtime = gather_draws_for_child_grid(
+                    draws, group_name, key, ages, times, location_id)
+                LOGGER.debug(f"Child prior found for {group_name} {key}")
+            elif group_name != "rate":
+                draw_value, draw_dage, draw_dtime = gather_draws_for_grid(draws, group_name, key, ages, times)
+                LOGGER.debug(f"Prior found for {group_name} {key}")
+            else:
+                LOGGER.debug(f"No prior found for {group_name} {key}")
+                continue
+
+            estimate_grid_parameters(prior_grid.value, draw_value, ages, times)
+            estimate_grid_parameters(prior_grid.dage, draw_dage, ages[:-1], times)
+            estimate_grid_parameters(prior_grid.dtime, draw_dtime, ages, times[:-1])
+
+
 def gather_draws_for_grid(draws, group_name, key, ages, times):
     # Gather data from incoming draws into an array of (draw, age, time)
     draw_data = np.zeros((len(draws), len(ages), len(times)))
@@ -263,6 +293,22 @@ def gather_draws_for_grid(draws, group_name, key, ages, times):
         for aidx, age in enumerate(ages):
             for tidx, time in enumerate(times):
                 draw_data[didx, aidx, tidx] = one_draw(age, time)
+
+    draw_data = draw_data.transpose([1, 2, 0])
+    draw_dage = np.diff(draw_data, n=1, axis=0)
+    draw_dtime = np.diff(draw_data, n=1, axis=1)
+    return draw_data, draw_dage, draw_dtime
+
+
+def gather_draws_for_child_grid(draws, group_name, key, ages, times, location_id):
+    # Gather data from incoming draws into an array of (draw, age, time)
+    draw_data = np.zeros((len(draws), len(ages), len(times)))
+    for didx in range(len(draws)):
+        underlying = draws[didx][group_name][key]
+        random_effect = draws[didx]["random_effect"][(key, location_id)]
+        for aidx, age in enumerate(ages):
+            for tidx, time in enumerate(times):
+                draw_data[didx, aidx, tidx] = underlying(age, time) * np.exp(random_effect(age, time))
 
     draw_data = draw_data.transpose([1, 2, 0])
     draw_dage = np.diff(draw_data, n=1, axis=0)
