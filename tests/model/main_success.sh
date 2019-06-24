951980a1a6a79ab84b3a3e2456fbe4eca5cdf6a0
Andrew Dolgert <adolgert@uw.edu>
2019-06-24 21:17:16 +0000

    modified shell file

--- tests/model/main_success.sh
+++ tests/model/main_success.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #$ -P proj_dismod_at
 #$ -N avas_food
+#$ -o /ihme/temp/sgeoutput/$USER/dismod
+#$ -j y
 #$ -q all.q
 #$ -l h_rt=4:00:00
-#$ -l fthread=2
+#$ -l fthread=1
 #$ -l m_mem_free=16G
 #$ -t 1-3580
-#$ -tc 20
+#$ -tc 32
 #$ -cwd
 # 3580
 source /ihme/code/adolgert/venv/cascade3.7/bin/activate
