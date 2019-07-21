ece007bdca275d2f4535a19c1bd88800f729a46e
Drew Dolgert <adolgert@uw.edu>
2019-07-20 18:34:32 -0700

    added a test, but it currently does little

--- tests/executor/test_estimate_locations.py
+++ tests/executor/test_estimate_locations.py
@@ -1,50 +1,35 @@
 import pytest
+from gridengineapp import entry
 from numpy.random import RandomState
 
+from cascade.executor.construct_model import construct_model
 from cascade.executor.covariate_description import create_covariate_specifications
 from cascade.executor.create_settings import create_local_settings
-from cascade.executor.construct_model import construct_model
+from cascade.executor.dismodel_main import DismodAT
+from cascade.executor.execution_context import make_execution_context
+from cascade.executor.priors_from_draws import set_priors_from_parent_draws
 from cascade.input_data.configuration.raw_input import validate_input_data_types
 from cascade.input_data.db.locations import location_hierarchy
-from cascade.executor.execution_context import make_execution_context
 from cascade.testing_utilities.fake_data import retrieve_fake_data
-from cascade.input_data.configuration.local_cache import LocalCache
-from cascade.executor.priors_from_draws import set_priors_from_parent_draws
 
 
-@pytest.mark.skip("for the main rewrite")
 @pytest.mark.parametrize("meid,mvid", [
     (None, 268613),
 ])
 def test_with_known_id(ihme, meid, mvid, tmp_path):
     """This runs the equivalent of dismodel_main.main"""
-    ec = make_execution_context()
     # no-upload keeps this from going to the databases when it's done.
-    args = ["z.db", "--no-upload", "--db-only"]
+    args = ["--no-upload", "--db-only", "--skip-cache",
+            "--base-directory", str(tmp_path)]
     if mvid:
-        args += ["--mvid", str(mvid), "--base-directory", str(tmp_path)]
+        args += ["--mvid", str(mvid)]
     elif meid:
         args += ["--meid", str(meid)]
     else:
         assert meid or mvid
-    plan = generate_plan(ec, parse_arguments(args))
-    local_cache = LocalCache(maxsize=200)
-    for task_id in plan.cascade_jobs:
-        job, this_location_work = plan.cascade_job(task_id)
-        # Change the tier by hand b/c the bundle creation would normally
-        # have run, but not for this test.
-        this_location_work.data_access.tier = 2
-        if job == "estimate_location:prepare_data":
-            prepare_data_for_estimate(ec, this_location_work, local_cache)
-        elif job == "estimate_location:construct_model":
-            construct_model_for_estimate_location(this_location_work, local_cache)
-        elif job == "estimate_location:initial_guess_from_fit_fixed":
-            assert False, "This option is disabled by default and shouldn't have run"
-        elif job == "estimate_location:compute_initial_fit":
-            compute_initial_fit(ec, this_location_work, local_cache)
-        elif job == "estimate_location:compute_draws_from_parent_fit":
-            break  # Do one, not the whole tree.
-        # else is a bundle setup.
+
+    app = DismodAT()
+    entry(app, args)
 
 
 @pytest.mark.skip("for the main rewrite")
