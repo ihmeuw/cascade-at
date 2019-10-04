82dcee4e0edc4ffd1647e8eb11a9b2238824da8f
mnorwood <mnorwood@uw.edu>
2019-10-04 16:54:44 -0700

    Commenting out the test that asserts CSMR in t3. There are no more model results.

--- tests/input_data/db/test_csmr.py
+++ tests/input_data/db/test_csmr.py
@@ -5,8 +5,11 @@ from cascade.input_data.db.csmr import _csmr_in_t3
 def test_csmr_check_in_t3_by_location(ihme):
     ec = make_execution_context()
     # This should be a list of location ids.
-    locs = _csmr_in_t3(ec, 267245)
-    assert locs == [80]
+    # TODO: Re-instate this test when we get model results and input
+    #  data uploaded for CSMR.
+    # locs = _csmr_in_t3(ec, 267245)
+    # assert locs == [80]
+
     # If the model version id doesn't exist, return an empty list.
     locs = _csmr_in_t3(ec, -3)
     assert not locs
