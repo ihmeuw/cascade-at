4ada8dc6ac81d488cf1aa0fb787f8450f7f75b83
mnorwood <mnorwood@uw.edu>
2019-10-14 11:23:37 -0700

    Adding elmo to the package list.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     extras_require={
         "testing": ["hypothesis", "pytest", "pytest-mock"],
         "documentation": ["sphinx", "sphinx_rtd_theme", "sphinx-autobuild", "sphinxcontrib-napoleon"],
-        "ihme_databases": ["db_tools", "db_queries"],
+        "ihme_databases": ["db_tools", "db_queries", "elmo"],
     },
     entry_points={
         "console_scripts": [
