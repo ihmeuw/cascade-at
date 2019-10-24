237350787bf4afb49e4f421cc8fcc8f970a9a17d
Marlena Bannick <mnorwood@uw.edu>
2019-10-24 16:09:04 -0700

    adding new base case

--- src/cascade_at/settings/base_case.py
+++ src/cascade_at/settings/base_case.py
@@ -3,16 +3,14 @@ BASE_CASE = {"model": {"random_seed": 139285335, "default_age_grid": "0 1 5 10 2
            "ode_step_size": 5, "minimum_meas_cv": 0, "rate_case": "iota_pos_rho_zero", "data_density": "log_gaussian",
            "constrain_omega": 0, "modelable_entity_id": 2005, "decomp_step_id": 3, "research_area": 2, "title": "test",
            "description": "<p>test<\/p>", "bundle_id": 173, "crosswalk_version_id": 5699, "drill": "cascade",
-           "split_sex": "most_detailed", "add_csmr_cause": 587, "model_version_id": 395834},
+           "split_sex": "most_detailed", "add_csmr_cause": 587, "model_version_id": 395837},
  "max_num_iter": {"fixed": 100, "random": 100}, "print_level": {"fixed": 5, "random": 0},
  "accept_after_max_steps": {"fixed": 5, "random": 5}, "students_dof": {"priors": 5, "data": 5},
- "log_students_dof": {"priors": 5, "data": 5}, "eta": {"priors": 0.01, "data": 0.01}, "config_version": "mnorwood",
+ "log_students_dof": {"priors": 5, "data": 5}, "eta": {"priors": 0.01, "data": 0.01}, "config_version": 1,
  "min_cv": [{"cascade_level_id": "most_detailed", "value": 0.4}], "rate": [{"age_time_specific": 1, "default": {
-    "value": {"density": "gaussian", "min": 0, "mean": 0, "max": 1, "std": 1, "eta": 1},
-    "dage": {"density": "gaussian", "min": 0, "mean": 0, "max": 1, "std": 1, "eta": 1},
-    "dtime": {"density": "gaussian", "min": 0, "mean": 0, "max": 1, "std": 1, "eta": 1}}, "rate": "iota",
-                                                                            "age_grid": "0 10 20 30 40 50 60 70 80 90 100",
-                                                                            "time_grid": "1990 1995 2000 2005 2010 2015 2016"}],
+    "value": {"density": "gaussian", "min": 1.0e-5, "mean": 1, "max": 1, "std": 1, "eta": 1},
+    "dage": {"density": "gaussian", "min": 1.0e-5, "mean": 1, "max": 1, "std": 1, "eta": 1},
+    "dtime": {"density": "gaussian", "min": 1.0e-5, "mean": 1, "max": 1, "std": 1, "eta": 1}}, "rate": "iota"}],
  "country_covariate": [
      {"age_time_specific": 0, "mulcov_type": "meas_value", "measure_id": 5, "country_covariate_id": 28,
       "transformation": 0}], "random_effect": [{"age_time_specific": 0, "default": {
