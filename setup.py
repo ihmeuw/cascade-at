e90677cf27bf229b0c3387f59455a6c28cabad84
Marlena Bannick <mnorwood@uw.edu>
2019-12-11 10:51:47 -0800

    Adding predict functionality to entry points

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     entry_points={'console_scripts': [
         'configure_inputs=cascade_at.executor.configure_inputs:main',
         'dismod_db=cascade_at.executor.dismod_db:main',
+        'format_upload=cascade_at.executor.format_upload:main',
         'cleanup=cascade_at.executor.cleanup:main',
         'run_cascade=cascade_at.executor.run:main'
     ]}
