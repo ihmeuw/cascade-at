1b2c7763577c914568570a5d25cbdf521c6aa4d6
Marlena Bannick <mnorwood@uw.edu>
2020-01-06 15:31:56 -0800

    Added covariate interpolation tests for exact ages and times.

--- tests/inputs/utilities/test_covariate_weighting.py
+++ tests/inputs/utilities/test_covariate_weighting.py
@@ -86,19 +86,58 @@ def a1():
 
 @pytest.fixture
 def time_wt(y0, y1):
-    return np.asarray([(2011-y0)/(2012-2011), (y1-2011)/(2012-2011)])
+    return
 
 
 @pytest.fixture
 def age_wt(a0, a1):
-    return np.asarray([(90-a0)/(90-85), 1, (a1-95)/(125-95)])
+    return
 
 
-def test_covariate_interpolation_over_time(covariate_interpolator, y0, y1, test_cov, test_pop, test_data, time_wt):
+df = pd.DataFrame(
+        [[2010, 31, 0.1],
+         [2010, 32, 0.2],
+         [2010, 235, 0.3],
+         [2011, 31, 0.35],
+         [2011, 32, 0.4],
+         [2011, 235, 0.5]],
+        columns=['year_id', 'age_group_id', 'mean_value']
+    )
+
+
+@pytest.mark.parametrize("age_lower,age_upper,time_lower,time_upper,answer", [
+    (85., 85.1, 2010., 2010.1, 0.1),
+    (90., 90.1, 2010., 2010.1, 0.2),
+    (95., 95.1, 2010., 2010.1, 0.3),
+    (85., 85.1, 2011., 2011.1, 0.35),
+    (90., 90.1, 2011., 2011.1, 0.4),
+    (95., 95.1, 2011., 2011.1, 0.5),
+])
+def test_exact_values_age_time(covariate_interpolator, age_lower, age_upper, time_lower, time_upper, answer):
+    assert np.allclose(
+        covariate_interpolator.interpolate(
+            loc_id=100,
+            sex_id=1,
+            age_lower=age_lower,
+            age_upper=age_upper,
+            time_lower=time_lower,
+            time_upper=time_upper
+        ), answer, atol=1e-10, rtol=1e-10
+    )
+
+
+@pytest.mark.parametrize("y0,y1", [
+    (2010.0, 2011.0),
+    (2010.0, 2011.5),
+    (2010.1, 2011.5),
+    (2011.0, 2011.5)
+])
+def test_covariate_interpolation_over_time(covariate_interpolator, y0, y1, test_cov, test_pop, test_data):
     data = test_data.copy()
     data[['time_lower', 'time_upper']] = y0, y1
     cov = test_cov[(test_cov.age_group_id == 32)]
     pop = test_pop[(test_pop.age_group_id == 32)]
+    time_wt = np.asarray([(2011 - y0) / (2012 - 2011), (y1 - 2011) / (2012 - 2011)])
     pop_wt = time_wt * pop.population.values
     weighted_cov = np.sum(cov.mean_value.values * pop_wt/pop_wt.sum())
     assert np.allclose(
@@ -113,11 +152,19 @@ def test_covariate_interpolation_over_time(covariate_interpolator, y0, y1, test_
     )
 
 
+@pytest.mark.parametrize("a0,a1", [
+    (87, 100),
+    (90, 95),
+    (90, 96),
+    (80, 100)
+])
 def test_covariate_interpolation_over_age(covariate_interpolator, a0, a1, test_cov, test_pop, test_data, age_wt):
     data = test_data.copy()
     data[['age_lower', 'age_upper']] = a0, a1
     cov = test_cov[(test_cov.year_id == 2010)]
     pop = test_pop[(test_pop.year_id == 2010)]
+
+    age_wt = np.asarray([(90 - a0) / (90 - 85), 1, (a1 - 95) / (125 - 95)])
     pop_wt = age_wt * pop.population.values
     weighted_cov = np.sum(cov.mean_value.values * pop_wt/pop_wt.sum())
     assert np.allclose(
@@ -132,14 +179,30 @@ def test_covariate_interpolation_over_age(covariate_interpolator, a0, a1, test_c
     )
 
 
-def test_covariate_interpolation_over_age_and_time(covariate_interpolator, y0, y1, a0, a1, test_cov, test_pop,
-                                                   test_data, age_wt, time_wt):
+@pytest.mark.parametrize("y0,y1", [
+    (2010.0, 2011.0),
+    (2010.0, 2011.5),
+    (2010.1, 2011.5),
+    (2011.0, 2011.5)
+])
+@pytest.mark.parametrize("a0,a1", [
+    (87, 100),
+    (90, 95),
+    (90, 96),
+    (80, 100)
+])
+def test_covariate_interpolation_over_age_and_time(covariate_interpolator, y0, y1, a0, a1,
+                                                   test_cov, test_pop, test_data):
     data = test_data.copy()
     data[['age_lower', 'age_upper']] = a0, a1
     data[['time_lower', 'time_upper']] = y0, y1
     cov = test_cov.copy()
     pop = test_pop.copy()
+
+    age_wt = np.asarray([(90 - a0) / (90 - 85), 1, (a1 - 95) / (125 - 95)])
+    time_wt = np.asarray([(2011 - y0) / (2012 - 2011), (y1 - 2011) / (2012 - 2011)])
     wt = np.outer(time_wt, age_wt)
+
     pop_wt = wt * pop.population.values.reshape(wt.shape)
     weighted_cov = np.sum(cov.mean_value.values.reshape(wt.shape) * pop_wt/pop_wt.sum())
     assert np.allclose(
