7335e5cfc584f5e53385ba2303010ebaede175c7
Marlena Bannick <mnorwood@uw.edu>
2020-01-07 12:54:14 -0800

    Shared fixtures for the input data in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
 import pytest
+import pandas as pd
+import numpy as np
+from types import SimpleNamespace
 
 import cascade_at.core.db
+from cascade_at.inputs.data import CrosswalkVersion
+from cascade_at.inputs.csmr import CSMR
+from cascade_at.inputs.asdr import ASDR
+from cascade_at.inputs.population import Population
+
 
 cascade_at.core.db.BLOCK_SHARED_FUNCTION_ACCESS = True
 
@@ -17,7 +25,7 @@ def pytest_addoption(parser):
                     help="run functions requiring access to fair cluster")
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def ihme(request):
     return IhmeDbFuncArg(request)
 
@@ -57,3 +65,93 @@ class DismodFuncArg:
     def __init__(self, request):
         if not (request.config.getoption("dismod") or request.config.getoption("ihme")):
             pytest.skip("specify --dismod or --ihme to run tests requiring Dismod")
+
+
+@pytest.fixture(scope='session')
+def Demographics():
+    d = SimpleNamespace()
+    d.age_group_id = list(range(2, 12))
+    d.location_id = [101]
+    d.sex_id = [2]
+    d.year_id = [1990, 1991]
+    return d
+
+
+@pytest.fixture(scope='session')
+def cv(Demographics, ihme):
+    cv = CrosswalkVersion(crosswalk_version_id=1, exclude_outliers=1,
+                          demographics=Demographics, conn_def='dismod-at-dev')
+    cv.raw = pd.DataFrame({
+        'underlying_nid': np.nan,
+        'nid': 230075,
+        'field_citation_value': '',
+        'source_type': '',
+        'location_name': 'Canada',
+        'location_id': 101,
+        'sex': 'Female',
+        'year_start': 1990,
+        'year_end': 1991,
+        'age_start': 0.0,
+        'age_end': 15.0,
+        'measure': 'incidence',
+        'mean': 4e-05,
+        'lower': 5e-05,
+        'upper': 3e-05,
+        'standard_error': 3e-06,
+        'cases': 100.,
+        'sample_size': 2000000.,
+        'unit_type': '',
+        'unit_value_as_published': 1,
+        'uncertainty_type_value': 95,
+        'representative_name': '',
+        'urbanicity_type': '',
+        'recall_type': '',
+        'recall_type_value': np.nan,
+        'sampling_type': '',
+        'group': np.nan,
+        'specificity': np.nan,
+        'group_review': np.nan,
+        'seq': 342686,
+        'crosswalk_parent_seq': 321982,
+        'variance': np.nan,
+        'effective_sample_size': 2000000.,
+        'design_effect': np.nan,
+        'is_outlier': 0,
+        'standardized.case.definition': '',
+        'serum_plasma': np.nan,
+        'orig_source': '',
+        'age_split': 0,
+        'standard_error_orig': 3e-06,
+        'mean_orig': 4e-05,
+        'input_type': '',
+        'uncertainty_type': '',
+        'underlying_field_citation_value': np.nan
+    }, index=[0])
+    return cv
+
+
+@pytest.fixture(scope='session')
+def csmr(Demographics, ihme):
+    csmr = CSMR(process_version_id=None, cause_id=587, demographics=Demographics,
+                decomp_step='step3', gbd_round_id=6)
+    csmr.raw = pd.DataFrame({
+        'age_group_id': 2,
+        'cause_id': 587,
+        'location_id': 101,
+        'measure_id': 1,
+        'metric_id': 3,
+        'sex_id': 2,
+        'year_id': 1990,
+        'acause': '',
+        'age_group_name': '',
+        'cause_name': '',
+        'expected': False,
+        'location_name': 'Canada',
+        'measure_name': '',
+        'metric_name': '',
+        'sex': 'Female',
+        'val': 5e-06,
+        'upper': 6e-06,
+        'lower': 2e-06
+    }, index=[0])
+    return csmr
