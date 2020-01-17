92c668c006a000d7572691ed1b2a11471cc5094d
Marlena Bannick <mnorwood@uw.edu>
2020-01-16 17:18:23 -0800

    Fixing the cluster project and time limits.

--- src/cascade_at/jobmon/resources.py
+++ src/cascade_at/jobmon/resources.py
@@ -2,7 +2,7 @@
 DEFAULT_EXECUTOR_PARAMETERS = {
     'm_mem_free': '30G',
     'num_cores': 5,
-    'max_runtime_seconds': 60*60*24,
+    'max_runtime_seconds': 60*60*24*4,
     'resource_scales': {
         'm_mem_free': 0.3,
         'max_runtime_seconds': 0.3
