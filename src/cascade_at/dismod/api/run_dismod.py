1b9a126c86df88a6eca05c4ec1d56f428c3b681b
Marlena Bannick <mnorwood@uw.edu>
2020-01-17 17:05:29 -0800

    Added new executable for sample simulate

--- src/cascade_at/dismod/api/run_dismod.py
+++ src/cascade_at/dismod/api/run_dismod.py
@@ -24,3 +24,26 @@ def run_dismod(dm_file, command):
     info.stderr = process.stderr.decode()
     
     return info
+
+
+def run_dismod_commands(dm_file, commands):
+    """
+    Runs multiple commands on a dismod file and returns the exit statuses.
+    Will raise an exception if it runs into an error.
+
+    Args:
+        dm_file:
+        commands:
+
+    Returns:
+
+    """
+    for c in commands:
+        process = run_dismod(dm_file=dm_file, command=c)
+        if process.exit_status:
+            LOG.error(f"{c} failed with exit_status {process.exit_status}:")
+            LOG.error(f"{process.stderr}")
+            raise
+        else:
+            print(process.stdout)
+            print(process.stderr)
