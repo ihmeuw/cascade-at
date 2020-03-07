6f635280a52c30e9c8ce3387a7e6128181ddad02
Marlena Bannick <mnorwood@uw.edu>
2020-03-06 17:23:21 -0800

    Posterior to priors filling the avgint table.

--- src/cascade_at/dismod/api/fill_extract_helpers/posterior_to_prior.py
+++ src/cascade_at/dismod/api/fill_extract_helpers/posterior_to_prior.py
@@ -1,11 +1,18 @@
+import pandas as pd
+
 from cascade_at.model.grid_alchemy import Alchemy
 from cascade_at.model.utilities.grid_helpers import integrand_grids, expand_grid
 from cascade_at.dismod.api.fill_extract_helpers.utils import vec_to_midpoint
+from cascade_at.dismod.constants import RateToIntegrand, IntegrandEnum, INTEGRAND_TO_WEIGHT
 
 
-def get_prior_avgint_dict(settings, integrands, sexes, locations, midpoint=True):
+def get_prior_avgint_grid(settings, integrands, sexes, locations, midpoint=False):
     """
-    Get a data frame to use with the AvgInt
+    Get a data frame to use for setting up posterior predictions on a grid.
+
+    Will still need to have covariates added to it, and prep data from
+    dismod.api.data_tables.prep_data_avgint to convert nodes and covariate names
+    before it can be input into the avgint table in a database.
 
     Args:
         settings: (cascade_at.settings.settings_configuration.SettingsConfiguration)
@@ -14,34 +21,44 @@ def get_prior_avgint_dict(settings, integrands, sexes, locations, midpoint=True)
         locations: (list of int)
         midpoint: (bool)
 
-    Returns: (pd.DataFrame)
+    Returns: (pd.DataFrame) with columns
+        "avgint_id", "integrand_id", "location_id", "weight_id", "subgroup_id",
+        "age_lower", "age_upper", "time_lower", "time_upper", "sex_id"
 
     """
-    prior_dfs = dict()
+    posterior_dfs = pd.DataFrame()
     alchemy = Alchemy(settings)
     grids = integrand_grids(alchemy=alchemy, integrands=integrands)
     for k, v in grids.items():
         if midpoint:
-            time_lower = vec_to_midpoint(v['time'])
-            time_upper = time_lower
-            age_lower = vec_to_midpoint(v['age'])
-            age_upper = age_lower
+            time = vec_to_midpoint(v['time'])
+            age = vec_to_midpoint(v['age'])
         else:
-            time_lower = v['time'][1:]
-            time_upper = v['time'][:-1]
-            age_lower = v['age'][1:]
-            age_upper = v['age'][:-1]
+            time = v['time']
+            age = v['age']
 
-        time_dict = dict(zip(time_lower, time_upper))
-        age_dict = dict(zip(age_lower, age_upper))
-
-        prior_dfs[k] = expand_grid({
-            'ages': age_lower,
-            'times': time_lower,
+        posterior_df = expand_grid({
+            'age_lower': age,
+            'time_lower': time,
             'location_id': locations,
             'sex_id': sexes
         })
-        prior_dfs[k]['time_upper'] = prior_dfs[k]['time_lower'].map(time_dict)
-        prior_dfs[k]['age_upper'] = prior_dfs[k]['age_lower'].map(age_dict)
+        posterior_df['time_upper'] = posterior_df['time_lower']
+        posterior_df['age_upper'] = posterior_df['age_lower']
 
-    return prior_dfs
+        posterior_df['rate'] = k
+        posterior_df['integrand'] = posterior_df['rate'].map(RateToIntegrand)
+        posterior_df['integrand_id'] = posterior_df['integrand'].apply(
+            lambda x: IntegrandEnum[x].value
+        )
+        posterior_df['weight_id'] = posterior_df["integrand"].apply(
+            lambda x: INTEGRAND_TO_WEIGHT[x].value
+        )
+        posterior_df['subgroup_id'] = 0
+
+        posterior_dfs = posterior_dfs.append(posterior_df)
+
+    return posterior_dfs[[
+        "avgint_id", "integrand_id", "location_id", "weight_id", "subgroup_id",
+        "age_lower", "age_upper", "time_lower", "time_upper", "sex_id"
+    ]]
---
+++ src/cascade_at/executor/prediction_statistics.py
@@ -0,0 +1,51 @@
+import logging
+from argparse import ArgumentParser
+
+from cascade_at.context.model_context import Context
+from cascade_at.dismod.api.dismod_io import DismodIO
+from cascade_at.dismod.api.fill_extract_helpers.data_tables import prep_data_avgint
+from cascade_at.dismod.api.fill_extract_helpers.posterior_to_prior import get_prior_avgint_grid
+from cascade_at.core.log import get_loggers, LEVELS
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument("-model-version-id", type=int, required=True)
+    parser.add_argument("-source-location", type=int, required=True)
+    parser.add_argument("-source-sex", type=int, required=True)
+    parser.add_argument("-target-locations", nargs="+", required=True, default=[], type=int)
+    parser.add_argument("-target-sexes", nargs="+", required=True, default=[], type=int)
+    parser.add_argument("--loglevel", type=str, required=False, default='info')
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    logging.basicConfig(level=LEVELS[args.loglevel])
+
+    context = Context(model_version_id=args.model_version_id)
+    inputs, alchemy, settings = context.read_inputs()
+
+    sourceDB = DismodIO(path=context.db_file(
+        location_id=args.source_location, sex_id=args.source_sex, make=False
+    ))
+
+    rates = [r.rate for r in settings.rate]
+    posterior_grid = get_prior_avgint_grid(
+        settings=settings,
+        integrands=rates,
+        sexes=args.target_sexes,
+        locations=args.target_locations,
+        midpoint=False
+    )
+    posterior_grid = inputs.add_covariates_to_data(df=posterior_grid)
+    posterior_grid = prep_data_avgint(
+        df=posterior_grid,
+        node_df=sourceDB.node,
+        covariate_df=sourceDB.covariate
+    )
+    sourceDB.avgint = posterior_grid
+
+
+if __name__ == '__main__':
+    main()
