307855548423787e0bfc55b4ef09167b494d50e1
Marlena Bannick <mnorwood@uw.edu>
2020-03-13 18:14:00 -0700

    Added table test

---
+++ tests/dismod/test_dmdismod_init.py
@@ -0,0 +1,135 @@
+"""
+This test uses Brad's getting_started_db
+example to create database tables and then make sure
+that we can run init on it using this code base to fill the tables.
+"""
+
+import pytest
+import pandas as pd
+import numpy as np
+
+from cascade_at.dismod.api.dismod_io import DismodIO
+from cascade_at.dismod.api.fill_extract_helpers.reference_tables import (
+    default_rate_table, default_integrand_table, construct_density_table
+)
+
+
+@pytest.fixture
+def dm(tmp_path):
+    return DismodIO(path=tmp_path / 'dismod-init.db')
+
+
+def test_fill_tables(dm):
+    unknown_omega_world = 1e-2
+    known_income_multiplier = -1e-3
+    adjusted_omega = unknown_omega_world * np.exp(known_income_multiplier * 1000.0)
+
+    dm.integrand = default_integrand_table()
+    dm.rate = default_rate_table()
+    dm.density = construct_density_table()
+
+    dm.age = pd.DataFrame({
+        'age_id': [0, 1],
+        'age': [0.0, 100.0]
+    })
+    dm.time = pd.DataFrame({
+        'time_id': [0, 1],
+        'time': [1995.0, 2015.0]
+    })
+    dm.node = pd.DataFrame({
+        'node_id': [0],
+        'node_name': ['world'],
+        'parent': ['']
+    })
+    dm.weight = pd.DataFrame({
+        'weight_id': [0],
+        'weight_name': ['constant'],
+        'n_age': [1],
+        'n_time': [1]
+    })
+    dm.covariate = pd.DataFrame({
+        'covariate_id': [0],
+        'covariate_name': ['income'],
+        'reference': [0],
+        'max_difference': [0]
+    })
+    dm.avgint = pd.DataFrame({
+        'avgint_id': [0],
+        'integrand_id': [0],
+        'node_id': [0],
+        'weight_id': [0],
+        'subgroup_id': [0],
+        'age_lower': [0],
+        'age_upper': [0],
+        'time_lower': [1995.],
+        'time_upper': [1995.]
+    })
+    dm.data = pd.DataFrame({
+        'data_id': [0],
+        'data_name': ['world'],
+        'integrand_id': [0],
+        'density_id': [1],
+        'node_id': [0],
+        'weight_id': [0],
+        'subgroup_id': [0],
+        'hold_out': [0],
+        'meas_value': [np.exp(-1 * adjusted_omega * 50.)],
+        'meas_std': [np.exp(-1 * adjusted_omega * 50.) / 20.],
+        'eta': [np.nan],
+        'nu': [np.nan],
+        'age_lower': [50.],
+        'age_upper': [50.],
+        'time_lower': [2000.],
+        'time_upper': [2000.]
+    })
+    dm.prior = pd.DataFrame({
+        'prior_id': [0, 1, 2],
+        'prior_name': ['prior_not_used', 'prior_omega_parent', 'prior_income_multiplier'],
+        'density_id': [0, 0, 0],
+        'lower': [np.nan, 1e-4, known_income_multiplier],
+        'upper': [np.nan, 1.0, known_income_multiplier],
+        'mean': [0.0, 1e-1, known_income_multiplier],
+        'std': np.nan,
+        'eta': np.nan,
+        'nu': np.nan
+    })
+    dm.smooth = pd.DataFrame({
+        'smooth_id': [0, 1],
+        'smooth_name': ['smooth_omega_parent', 'smooth_income_multiplier'],
+        'n_age': [2],
+        'n_time': [2],
+        'mulstd_value_prior_id': [0, 1, 2]
+    })
+    dm.smooth_grid = pd.DataFrame({
+        'smooth_grid_id': [0, 1],
+        'smooth_id': [0, 1],
+        'age_id': [0, 0],
+        'time_id': [0, 0]
+    })
+    dm.rate = pd.DataFrame({
+        'rate_id': [0],
+        'rate_name': 'omega',
+        'parent_smooth_id': [0]
+    })
+    dm.mulcov = pd.DataFrame({
+        'mulcov_id': [0],
+        'mulcov_type': 'rate_value',
+        'rate_id': [0],
+        'integrand_id': [0],
+        'covariate_id': [0],
+        'group_id': [0],
+        'subgroup_smooth_id': [1]
+    })
+    dm.option = pd.DataFrame({
+        'option_id': [0, 1, 2, 3],
+        'option_name': ['parent_node_id', 'ode_step_size', 'age_avg_split', 'rate_case'],
+        'option_value': ['0', '10.0', '5.0', 'iota_zero_rho_zero']
+    })
+    dm.subgroup = pd.DataFrame({
+        'subgroup_id': [0],
+        'subgroup_name': ['world'],
+        'group_id': [0],
+        'group_name': ['world']
+    })
+
+
