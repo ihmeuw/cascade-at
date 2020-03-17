32d10ef676f7941479827f5e5d7012011758bf01
Marlena Bannick <mnorwood@uw.edu>
2020-03-17 15:40:46 -0700

    Updated test case

--- tests/dismod/api/test_dismod_fill.py
+++ tests/dismod/api/test_dismod_fill.py
@@ -321,8 +321,8 @@ def integrand():
          'integrand_name': {0: 'Sincidence', 1: 'remission', 2: 'mtexcess', 3: 'mtother', 4: 'mtwith', 5: 'susceptible',
                             6: 'withC', 7: 'prevalence', 8: 'Tincidence', 9: 'mtspecific', 10: 'mtall',
                             11: 'mtstandard', 12: 'relrisk'},
-         'minimum_meas_cv': {0: 0.0, 1: 0.0, 2: 0.0, 3: 0.0, 4: 0.0, 5: 0.0, 6: 0.0, 7: 0.0, 8: 0.0, 9: 0.0, 10: 0.0,
-                             11: 0.0, 12: 0.0}}
+         'minimum_meas_cv': {0: 0.1, 1: 0.1, 2: 0.1, 3: 0.1, 4: 0.1, 5: 0.1, 6: 0.1, 7: 0.1, 8: 0.1, 9: 0.1, 10: 0.1,
+                             11: 0.1, 12: 0.1}}
     )
 
 
