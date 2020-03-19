aeb1dffeb8dfa6a6e87f5702513315a7a8141f26
Marlena Bannick <mnorwood@uw.edu>
2020-03-18 17:35:46 -0700

    Formatting.

--- tests/dismod/api/test_dismod_extractor.py
+++ tests/dismod/api/test_dismod_extractor.py
@@ -1,6 +1,5 @@
 import pytest
 from pathlib import Path
-import pandas as pd
 import numpy as np
 import os
 
@@ -11,7 +10,7 @@ from cascade_at.dismod.api.dismod_extractor import DismodExtractor
 
 
 @pytest.fixture(scope='module')
-def ex(mi, settings, temp_directory):
+def ex(mi, settings, temp_directory, dismod):
     alchemy = Alchemy(settings)
     print(temp_directory)
     d = DismodFiller(
@@ -42,11 +41,13 @@ def test_get_predictions(ex):
     d = DismodExtractor(path=Path('extractor.db'))
     pred = d.get_predictions()
     assert len(pred) == 33
-    assert all(pred.columns == ['predict_id', 'sample_index', 'avgint_id', 'avg_integrand',
-       'integrand_id', 'node_id', 'weight_id', 'subgroup_id', 'age_lower',
-       'age_upper', 'time_lower', 'time_upper', 'c_age_group_id',
-       'c_location_id', 'c_sex_id', 'c_year_id', 'x_0', 'x_1', 'x_2',
-       'integrand_name', 'minimum_meas_cv', 'rate'])
+    assert all(pred.columns == [
+        'predict_id', 'sample_index', 'avgint_id', 'avg_integrand',
+        'integrand_id', 'node_id', 'weight_id', 'subgroup_id', 'age_lower',
+        'age_upper', 'time_lower', 'time_upper', 'c_age_group_id',
+        'c_location_id', 'c_sex_id', 'c_year_id', 'x_0', 'x_1', 'x_2',
+        'integrand_name', 'minimum_meas_cv', 'rate'
+    ])
     assert all(pred.age_lower == 0.0)
     assert all(pred.age_upper == 0.01917808)
     assert all(pred.integrand_id == np.repeat(list(range(0, 11)), 3))
@@ -60,8 +61,10 @@ def test_format_for_ihme():
     d = DismodExtractor(path=Path('extractor.db'))
     pred = d.format_predictions_for_ihme()
     assert len(pred) == 36
-    assert all(pred.columns == ['location_id', 'age_group_id', 'year_id', 'sex_id', 'measure_id',
-       'mean', 'upper', 'lower'])
+    assert all(pred.columns == [
+        'location_id', 'age_group_id', 'year_id', 'sex_id', 'measure_id',
+        'mean', 'upper', 'lower'
+    ])
     assert all(pred.location_id == np.tile(list(range(70, 73)), 12))
     assert all(pred.sex_id == 2)
     assert all(pred.age_group_id == 2)
