4ffb1cde8cd12393249bc17627f99d80b18d1824
Marlena Bannick <mnorwood@uw.edu>
2020-06-12 16:48:23 -0700

    Encoding function for arguments on the command line for option dict.

--- src/cascade_at/context/arg_utils.py
+++ src/cascade_at/context/arg_utils.py
@@ -1,3 +1,7 @@
+from typing import Dict, Any, List
+
+from cascade_at.core import CascadeATError
+
 
 type_mappings = {
     'int': int,
@@ -6,21 +10,50 @@ type_mappings = {
 }
 
 
-def parse_options(option_list):
-    """
-    Parse a KEY=VALUE=TYPE command line arg
-    that comes in a list
+class OptionParsingError(CascadeATError):
+    """Raised when there is an issue parsing an option dict for the command line."""
+    pass
 
-    :param option_list: List[str]
-    :return: dictionary of options that can be passed to the dismod database
+
+def parse_options(option_list: List[str]):
+    """
+    Parse a key=value=type command line arg
+    that comes in a list.
+
+    Returns
+    -------
+    Dictionary of options with the correct types.
     """
     d = dict()
     for o in option_list:
         o = o.split('=')
+        if len(o) != 3:
+            raise OptionParsingError("Not enough elements in the parsed options. Need 3 elements.")
         key = o[0]
         val = o[1]
+        if o[2] not in type_mappings:
+            raise OptionParsingError(f"Unknown option type {o[2]}.")
         type_func = type_mappings[o[2]]
         d.update({key: type_func(val)})
+    return d
+
+
+def encode_options(options: Dict[str, Any]):
+    """
+    Encode an option dict into a command line string that cascade_at can understand.
+
+    Returns
+    -------
+    List of strings that can be passed to the command line..
+    """
+    d = list()
+    rev_dict = {v: k for k, v in type_mappings.items()}
+    for k, v in options.items():
+        t = type(v)
+        if t not in rev_dict:
+            raise OptionParsingError(f"Unknown option type {t}.")
+        arg = f'{k}={v}={rev_dict[t]}'
+        d.append(arg)
     return d
 
 
--- tests/context/test_arg_utils.py
+++ tests/context/test_arg_utils.py
@@ -1,4 +1,7 @@
-from cascade_at.context.arg_utils import parse_options, parse_commands
+import numpy as np
+import pytest
+
+from cascade_at.context.arg_utils import parse_options, parse_commands, encode_options, OptionParsingError
 
 
 def test_options():
@@ -10,7 +13,26 @@ def test_options():
     assert option_dict['foobar'] == 1.0
 
 
+def test_encode_options():
+    options = {'foo': 'hello', 'bar': 1, 'foobar': 1.0}
+    encoded = encode_options(options)
+    assert encoded[0] == 'foo=hello=str'
+    assert encoded[1] == 'bar=1=int'
+    assert encoded[2] == 'foobar=1.0=float'
+
+
+def test_invalid_encode_options():
+    options = {'foo': np.array([1, 2])}
+    with pytest.raises(OptionParsingError):
+        encode_options(options)
+
+
+def test_invalid_parse_options():
+    options = ['foo=[1, 2]=list']
+    with pytest.raises(OptionParsingError):
+        parse_options(options)
+
+
 def test_commands():
     assert parse_commands(['init']) == ['init']
     assert parse_commands(['init', 'fit-fixed']) == ['init', 'fit fixed']
-
