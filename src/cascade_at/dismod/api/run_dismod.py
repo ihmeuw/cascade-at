520af387f90d58ea747ea5ffdcdfa211901a9611
Greg Anderson <gma1@uw.edu>
2020-10-20 11:01:37 -0700

    Avoid writing these log messages without content.

--- src/cascade_at/dismod/api/run_dismod.py
+++ src/cascade_at/dismod/api/run_dismod.py
@@ -65,7 +65,9 @@ def run_dismod_commands(dm_file: str, commands: List[str], sys_exit=True):
                 except RuntimeError:
                     sys.exit(process.exit_status)
         else:
-            LOG.info(f"{process.stdout}")
-            LOG.info(f"{process.stderr}")
+            if process.stdout:
+                LOG.info(f"{process.stdout}")
+            if process.stderr:
+                LOG.info(f"{process.stderr}")
             processes.update({c: process})
     return processes
