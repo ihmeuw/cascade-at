7e87ffba5d50193d0330cbfa5dea6281dc0e0f46
zhengp0 <zhengp@uw.edu>
2020-11-06 17:42:00 -0800

    minor dummy change to fix the PR.

--- tests/dismod_execution/test_case_1.py
+++ tests/dismod_execution/test_case_1.py
@@ -2,7 +2,7 @@ import pytest
 from . import example_db
 from . import dismod_tests
 
-print ('Case 1: Location fixed and random effects.')
+print('Case 1: Location fixed and random effects.')
 
 use_group_mulcov = False
 file_name = 'example.db'
@@ -16,24 +16,25 @@ config = {'sex_effect': False,
 
 truth, prior, node_effects, group_effects = dismod_tests.test_setup(use_group_mulcov)
 
-db_kwds = dict(test_config = config,
-               truth = truth,
-               prior = prior,
-               node_effects = node_effects,
-               subgroup_effects = group_effects,
-               tol_fixed = dismod_tests.tol_fixed,
-               tol_random = dismod_tests.tol_random)
+db_kwds = dict(test_config=config,
+               truth=truth,
+               prior=prior,
+               node_effects=node_effects,
+               subgroup_effects=group_effects,
+               tol_fixed=dismod_tests.tol_fixed,
+               tol_random=dismod_tests.tol_random)
 
-def test_1(dismod, assert_correct = True):
 
+def test_1(dismod, assert_correct=True):
     db = example_db.example_db(file_name, **db_kwds)
     success, db = dismod_tests.run_test(file_name, config, truth)
 
-    if success: print ('Dismod_AT succeeded -- that is the correct result.')
+    if success: print('Dismod_AT succeeded -- that is the correct result.')
     if assert_correct:
         assert success
 
-def test_2(dismod, assert_correct = True):
+
+def test_2(dismod, assert_correct=True):
     # For the asymptotic statistics to work, the parent rate density must be something other than uniform
     prior['parent_density'] = 'gaussian'
     prior['parent_std'] = 1000
@@ -41,10 +42,11 @@ def test_2(dismod, assert_correct = True):
     example_db.example_db(file_name, **db_kwds)
     success, db = dismod_tests.run_test(file_name, config, truth, test_asymptotic=True)
 
-    if success: print ('Dismod_AT fit with setup for asymptotic statistics succeeded -- that is the correct result.')
+    if success: print('Dismod_AT fit with setup for asymptotic statistics succeeded -- that is the correct result.')
     if assert_correct:
         assert success
 
+
 if __name__ == '__main__':
-    test_1(None, assert_correct = False)
-    test_2(None, assert_correct = False)
+    test_1(None, assert_correct=False)
+    test_2(None, assert_correct=False)
