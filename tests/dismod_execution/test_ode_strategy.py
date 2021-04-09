f396acc93f1cf70654dbeabad4034d4068adb4dc
Greg Anderson <gma1@uw.edu>
2021-04-08 18:18:48 -0700

    Initial checkin -- not a very good test yet.

---
+++ tests/dismod_execution/test_ode_strategy.py
@@ -0,0 +1,56 @@
+import pytest
+import os
+if __name__ == '__main__':
+    import example_db
+    import dismod_tests
+else:
+    from . import example_db
+    from . import dismod_tests
+
+print('Case 1: New fitting strategy -- first fit the non-ODE integrands, use that solution to initialize the ODE integrand fit.')
+
+use_group_mulcov = False
+file_name = 'ode_example.db'
+
+config = {'sex_effect': True,
+          'node_effects': True,
+          'group_effects': False,
+          'use_group_mulcov': use_group_mulcov,
+          'include_group_data': False,
+          'zero_sum_mulcov': False}
+
+truth, prior, node_effects, group_effects = dismod_tests.test_setup(use_group_mulcov)
+
+
+db_kwds = dict(test_config=config,
+               truth=truth,
+               prior=prior,
+               node_effects=node_effects,
+               subgroup_effects=group_effects,
+               tol_fixed=dismod_tests.tol_fixed,
+               tol_random=dismod_tests.tol_random)
+
+
+def test_1(dismod, assert_correct=True):
+    db = example_db.example_db(file_name, **db_kwds)
+    # FIXME -- hacks
+    cov = db.covariate
+    cov.loc[cov.covariate_name == 'one', 'c_covariate_name'] = 's_one'
+    cov.loc[cov.covariate_name == 'sex', 'c_covariate_name'] = 's_sex'
+    cov.loc[cov.covariate_name == 'one', 'covariate_name'] = 'x_0'
+    cov.loc[cov.covariate_name == 'sex', 'covariate_name'] = 'x_1'
+    db.covariate = cov
+    try:
+        os.system('dmdismod')
+        os.system('dmdismod --help')
+        os.system(f'dmdismod {db.path} ODE init')
+        os.system(f'dmdismod {db.path} ODE fit')
+        os.system(f'dmdismod {db.path} ODE students')
+        success = True
+    except:
+        success = False
+    if assert_correct:
+        assert success
+
+if __name__ == '__main__':
+    test_1(None, assert_correct=False)
