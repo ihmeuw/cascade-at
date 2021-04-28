4d8b3f75f4a8ccfab5c52a9bbb756ed78f13b858
Greg Anderson <gma1@uw.edu>
2021-04-28 11:53:58 -0700

    Initial checkin.

---
+++ src/cascade_at/executor/plot_residuals.py
@@ -0,0 +1,100 @@
+import sys
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from cascade_at.dismod.api.dismod_io import DismodIO
+import matplotlib.backends.backend_pdf
+from matplotlib import pyplot as plt
+plt.interactive(1)
+
+def parse_args():
+    import argparse
+    from distutils.util import strtobool as str2bool
+    parser = argparse.ArgumentParser()
+    name_string = "-filename" if sys.argv[0] == '' else "filename"
+    parser.add_argument(name_string, type=str, help="Dismod_AT cascade dbs directory")
+    parser.add_argument("-l", "--location-ids", type = int, nargs='+', default = None,
+                        help="Location ids to plot, default = all")
+    parser.add_argument("-i", "--integrands", type = str, nargs='+', default = None,
+                        help = f"Integrands to plot, default = all")
+    args = parser.parse_args()
+    return args
+
+def plot(residuals, path, integrands = None):
+    if integrands:
+        integrand_names = integrands
+    else:
+        integrand_names = residuals.integrand_name.unique()
+    for n in integrand_names:
+        integrand = residuals.loc[residuals.integrand_name == n]
+        fig, ax = plt.subplots(1, 1, tight_layout=True)
+        l = len(residuals.c_parent_location_id.unique())
+        m = len(integrand.c_parent_location_id.unique())
+        plt.title(f"{n.capitalize()} Fit Summary for {m} of {l} Locations With Data")
+        plt.xlabel('Weighted Residual')
+        plt.ylabel('Count')
+        r = integrand.weighted_residual
+        n_bins = max(100, int(len(r)/100))
+        ax.hist(r, bins=n_bins)
+        pdf_file = path / (n + '.pdf')
+        pdf = matplotlib.backends.backend_pdf.PdfPages(pdf_file)
+        print (f"Saving plot {pdf_file}")
+        pdf.savefig( fig )
+
+def collect(dbs, location_ids = None):
+    residuals = pd.DataFrame()
+    i = -1
+    if location_ids:
+        dbs = [p for p in dbs if int(p.parts[-3]) in location_ids]
+    for p in dbs:
+        global db
+        db = DismodIO(p)
+        try:
+            db.option
+            loc,sex = map(int, p.parts[-3:-1])
+            fit = (db.data_subset.merge(db.data, how='left')
+                   .merge(db.fit_data_subset, left_on = 'data_subset_id', right_on = 'fit_data_subset_id')
+                   .merge(db.node, how='left')
+                   .merge(db.integrand, how='left'))
+            cov_names = {f'x_{row.covariate_id}': row.c_covariate_name
+                         for i, row in db.covariate[['covariate_id', 'c_covariate_name']].iterrows()}
+            fit.rename(columns = cov_names, inplace=True)
+            fit['c_parent_location_id'] = loc
+            cols = (['c_parent_location_id', 'c_location_id', 'integrand_name', 'data_name',
+                     'age_lower', 'age_upper', 'time_lower', 'time_upper', 'weighted_residual']
+                    + list(cov_names.values()))
+            residuals = residuals.append(fit[cols])
+            i += 1
+            print (i, f'sex: {sex}, location: {loc}')
+        except:
+            continue
+    return residuals
+
+def main():
+    global args
+    args = parse_args()
+    path = Path(args.filename).expanduser()
+    assert path.is_dir()
+
+    dbs = list(path.glob('**/*/dismod.db'))
+    mvid,dbs_str = dbs[0].parts[-5:-3]
+    assert dbs_str == 'dbs', f'The path {path} does not specify the root of the cascade data structure for mvid {mvid}'
+    print (f"Accumulating the fit residuals for the entire cascade execution of mvid {mvid}.")
+
+    residuals = collect(dbs, location_ids = args.location_ids)
+    plot(residuals, path, integrands = args.integrands)
+
+    return residuals
+
+if __name__ == '__main__':
+    """
+    if not sys.argv[0]:
+        mvid_path = '/Users/gma/ihme/epi/at_cascade/data/475588/dbs'
+        superregions = [str(i) for i in [4, 31, 64, 103, 137, 158, 166]]
+        sys.argv = ['plot_residuals', mvid_path, '--location-ids'] + superregions
+    """
+    residuals = main()
+
+
+       
